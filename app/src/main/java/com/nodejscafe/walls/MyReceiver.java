9451e0f8d209fba0cff671445f07d524015a1e86
raajatk <rajatkatiyar2009@gmail.com>
2018-08-04 20:05:58 +0530

    added location listener

--- app/src/main/java/com/nodejscafe/walls/MyReceiver.java
+++ app/src/main/java/com/nodejscafe/walls/MyReceiver.java
@@ -1,11 +1,22 @@
 package com.nodejscafe.walls;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
 import android.app.AlarmManager;
 import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.telephony.TelephonyManager;
 import android.util.Log;
 
 import org.json.JSONArray;
@@ -19,17 +30,75 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.List;
+import java.util.Locale;
+
+import static android.content.Context.TELEPHONY_SERVICE;
 
 public class MyReceiver extends BroadcastReceiver {
+    LocationManager locationManager;
+    Context thisContext;
+    public boolean isGPSEnabled;
+    public boolean isNetworkEnabled;
 
     @Override
     public void onReceive(Context context, Intent intent) {
         // TODO: This method is called when the BroadcastReceiver is receiving
         // an Intent broadcast.
-        Log.w(MyReceiver.class.getSimpleName(), "Received the Broadcast");
-        new RestTask().execute("http://192.168.43.10:2000/api/v1.0/nodejscafe/test");
+        thisContext = context;
+        GPSTracker mGPS = new GPSTracker(thisContext);
+        Location loc=null;
+
+        final TelephonyManager tm = (TelephonyManager) thisContext.getSystemService(TELEPHONY_SERVICE);
+        if (ActivityCompat.checkSelfPermission(thisContext, Manifest.permission.READ_SMS) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(thisContext, Manifest.permission.READ_PHONE_NUMBERS) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(thisContext, Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        final String phoneNumber = tm.getLine1Number();
+        Log.w("PHONE::",phoneNumber+"  "+tm.getSimOperatorName()+"  "+tm.getSimOperator()+"  "+tm.getSimSerialNumber()+"  "+tm.getSubscriberId());
 
 
+        if(mGPS.canGetLocation ){
+            loc = mGPS.getLocation();
+            //String s1 = "Lat "+mGPS.getLatitude()+" Lon "+mGPS.getLongitude();
+            if(loc!=null){
+                Log.w("FOUND THIS",String.valueOf(loc));
+            }else{
+                Log.w("ERROR ","NULL");
+            }
+
+
+
+//            String cityName = null;
+//            Geocoder gcd = new Geocoder(thisContext,Locale.getDefault());
+//            List<Address> addresses;
+//            try {
+//                addresses = gcd.getFromLocation(loc.getLatitude(),
+//                        loc.getLongitude(), 1);
+//                if (addresses.size() > 0) {
+//                    System.out.println(addresses.get(0).getLocality());
+//                    cityName = addresses.get(0).getLocality();
+//                    Log.w("ADD "+" ", String.valueOf(addresses.get(0)));
+//                }
+//            }
+//            catch (IOException e) {
+//                e.printStackTrace();
+//            }
+        }else{
+//            text.setText("Unabletofind");
+            Log.w("FOUND THIS","NULLLLLLL");
+        }
+
+//        Log.w(MyReceiver.class.getSimpleName(), "Received the Broadcast");
+        new RestTask().execute("http://192.168.43.10:2000/api/v1.0/get_location/"+String.valueOf(loc));
+//
+//        sendLocation(context);
         Intent myIntent = new Intent(context, MyReceiver.class);
         PendingIntent pendingIntent = PendingIntent.getBroadcast(context, 0, myIntent, PendingIntent.FLAG_UPDATE_CURRENT);
         AlarmManager alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
@@ -37,12 +106,18 @@ public class MyReceiver extends BroadcastReceiver {
 
     }
 
-    public class RestTask extends AsyncTask<String,String,String>{
+    public void sendLocation(Context context) {
+
+        Log.w("LOCATION: LATITUDE ", String.valueOf(12));
+        Log.w("LOCATION: LONGITUDE ", String.valueOf(0.69));
+    }
+
+    public class RestTask extends AsyncTask<String, String, String> {
 
         @Override
         protected String doInBackground(String... params) {
-            HttpURLConnection connection=null;
-            BufferedReader reader=null;
+            HttpURLConnection connection = null;
+            BufferedReader reader = null;
 
             try {
                 URL url = new URL(params[0]);
@@ -56,9 +131,9 @@ public class MyReceiver extends BroadcastReceiver {
 
                 StringBuffer buffer = new StringBuffer();
 
-                String line ="";
+                String line = "";
 
-                while ((line=reader.readLine())!=null){
+                while ((line = reader.readLine()) != null) {
                     buffer.append(line);
                 }
 
@@ -69,11 +144,11 @@ public class MyReceiver extends BroadcastReceiver {
             } catch (IOException e) {
                 e.printStackTrace();
             } finally {
-                if(connection!=null){
+                if (connection != null) {
                     connection.disconnect();
                 }
 
-                if(reader!=null){
+                if (reader != null) {
                     try {
                         reader.close();
                     } catch (IOException e) {
@@ -89,15 +164,230 @@ public class MyReceiver extends BroadcastReceiver {
         @Override
         protected void onPostExecute(String result) {
             // execution of result of Long time consuming operation
-            if(result!=null){
+            if (result != null) {
                 Log.w("the results are ", result);
-            }else{
-                Log.w("ERROR: ","Something Went Wrong at Server");
+            } else {
+                Log.w("ERROR: ", "Something Went Wrong at Server");
             }
 
 
         }
     }
+
+
+    public final class GPSTracker implements LocationListener {
+
+        private final Context mContext;
+
+        // flag for GPS status
+        public boolean isGPSEnabled = false;
+
+        // flag for network status
+        boolean isNetworkEnabled = false;
+
+        // flag for GPS status
+        boolean canGetLocation = false;
+
+        Location location; // location
+        double latitude; // latitude
+        double longitude; // longitude
+
+        // The minimum distance to change Updates in meters
+        private static final long MIN_DISTANCE_CHANGE_FOR_UPDATES = 1; // 10 meters
+
+        // The minimum time between updates in milliseconds
+        private static final long MIN_TIME_BW_UPDATES = 1; // 1 minute
+
+        // Declaring a Location Manager
+        protected LocationManager locationManager;
+
+        public GPSTracker(Context context) {
+            this.mContext = context;
+            getLocation();
+        }
+
+        /**
+         * Function to get the user's current location
+         *
+         * @return
+         */
+        public Location getLocation() {
+            try {
+                locationManager = (LocationManager) mContext
+                        .getSystemService(Context.LOCATION_SERVICE);
+
+                // getting GPS status
+                isGPSEnabled = locationManager
+                        .isProviderEnabled(LocationManager.GPS_PROVIDER);
+
+                Log.v("isGPSEnabled", "=" + isGPSEnabled);
+
+                // getting network status
+                isNetworkEnabled = locationManager
+                        .isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+
+                Log.v("isNetworkEnabled", "=" + isNetworkEnabled);
+
+                if (isGPSEnabled == false && isNetworkEnabled == false) {
+                    // no network provider is enabled
+                } else {
+                    this.canGetLocation = true;
+                    if (isNetworkEnabled) {
+                        location = null;
+                        if (ActivityCompat.checkSelfPermission(thisContext, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(thisContext, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                            // TODO: Consider calling
+                            //    ActivityCompat#requestPermissions
+                            // here to request the missing permissions, and then overriding
+                            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                            //                                          int[] grantResults)
+                            // to handle the case where the user grants the permission. See the documentation
+                            // for ActivityCompat#requestPermissions for more details.
+                            Log.w("ERROR","Returning Null");
+                            return null;
+                        }
+                        locationManager.requestLocationUpdates(
+                                LocationManager.NETWORK_PROVIDER,
+                                MIN_TIME_BW_UPDATES,
+                                MIN_DISTANCE_CHANGE_FOR_UPDATES, this);
+                        Log.d("Network", "Network");
+                        if (locationManager != null) {
+                            location = locationManager
+                                    .getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+                            Log.w(" network provider ",String.valueOf(location));
+                            if (location != null) {
+                                latitude = location.getLatitude();
+                                longitude = location.getLongitude();
+
+                                String s1 = "Lat "+latitude+" Lon "+longitude;
+
+                            }
+                        }
+                    }
+                    // if GPS Enabled get lat/long using GPS Services
+                    if (isGPSEnabled) {
+                        location=null;
+                        if (location == null) {
+                            locationManager.requestLocationUpdates(
+                                    LocationManager.GPS_PROVIDER,
+                                    MIN_TIME_BW_UPDATES,
+                                    MIN_DISTANCE_CHANGE_FOR_UPDATES, this);
+                            Log.d("GPS Enabled", "GPS Enabled");
+                            if (locationManager != null) {
+                                location = locationManager
+                                        .getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                                if (location != null) {
+                                    latitude = location.getLatitude();
+                                    longitude = location.getLongitude();
+                                }
+                            }
+                        }
+                    }
+                }
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            //Log.w("at sending location ",String.valueOf(location));
+            return location;
+        }
+
+        /**
+         * Stop using GPS listener Calling this function will stop using GPS in your
+         * app
+         * */
+        public void stopUsingGPS() {
+            if (locationManager != null) {
+                locationManager.removeUpdates(GPSTracker.this);
+            }
+        }
+
+        /**
+         * Function to get latitude
+         * */
+        public double getLatitude() {
+            if (location != null) {
+                latitude = location.getLatitude();
+            }
+
+            // return latitude
+            return latitude;
+        }
+
+        /**
+         * Function to get longitude
+         * */
+        public double getLongitude() {
+            if (location != null) {
+                longitude = location.getLongitude();
+            }
+
+            // return longitude
+            return longitude;
+        }
+
+        /**
+         * Function to check GPS/wifi enabled
+         *
+         * @return boolean
+         * */
+        public boolean canGetLocation() {
+            return this.canGetLocation;
+        }
+
+        /**
+         * Function to show settings alert dialog On pressing Settings button will
+         * lauch Settings Options
+         * */
+        public void showSettingsAlert() {
+//            AlertDialog.Builder alertDialog = new AlertDialog.Builder(mContext);
+//
+//            // Setting Dialog Title
+//            alertDialog.setTitle("GPS is settings");
+//
+//            // Setting Dialog Message
+//            alertDialog
+//                    .setMessage("GPS is not enabled. Do you want to go to settings menu?");
+//
+//            // On pressing Settings button
+//            alertDialog.setPositiveButton("Settings",
+//                    new DialogInterface.OnClickListener() {
+//                        public void onClick(DialogInterface dialog, int which) {
+//                            Intent intent = new Intent(
+//                                    Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+//                            mContext.startActivity(intent);
+//                        }
+//                    });
+//
+//            // on pressing cancel button
+//            alertDialog.setNegativeButton("Cancel",
+//                    new DialogInterface.OnClickListener() {
+//                        public void onClick(DialogInterface dialog, int which) {
+//                            dialog.cancel();
+//                        }
+//                    });
+//
+//            // Showing Alert Message
+//            alertDialog.show();
+        }
+
+        @Override
+        public void onLocationChanged(Location location) {
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+        }
+
+    }
+
 
 }
 
