44d20dc3da44dd524fc0c36545d7543a00552fce
raajatk <rajatkatiyar2009@gmail.com>
2018-08-04 15:41:05 +0530

    hit api using async task

--- app/src/main/java/com/nodejscafe/walls/MyReceiver.java
+++ app/src/main/java/com/nodejscafe/walls/MyReceiver.java
@@ -5,7 +5,20 @@ import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class MyReceiver extends BroadcastReceiver {
 
@@ -14,10 +27,77 @@ public class MyReceiver extends BroadcastReceiver {
         // TODO: This method is called when the BroadcastReceiver is receiving
         // an Intent broadcast.
         Log.w(MyReceiver.class.getSimpleName(), "Received the Broadcast");
+        new RestTask().execute("http://192.168.43.10:2000/api/v1.0/nodejscafe/test");
+
 
         Intent myIntent = new Intent(context, MyReceiver.class);
         PendingIntent pendingIntent = PendingIntent.getBroadcast(context, 0, myIntent, PendingIntent.FLAG_UPDATE_CURRENT);
         AlarmManager alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
         alarmManager.set(alarmManager.RTC_WAKEUP, System.currentTimeMillis() + (5000), pendingIntent);
+
     }
+
+    public class RestTask extends AsyncTask<String,String,String>{
+
+        @Override
+        protected String doInBackground(String... params) {
+            HttpURLConnection connection=null;
+            BufferedReader reader=null;
+
+            try {
+                URL url = new URL(params[0]);
+                connection = (HttpURLConnection) url.openConnection();
+
+                connection.connect();
+
+                InputStream stream = connection.getInputStream();
+
+                reader = new BufferedReader(new InputStreamReader(stream));
+
+                StringBuffer buffer = new StringBuffer();
+
+                String line ="";
+
+                while ((line=reader.readLine())!=null){
+                    buffer.append(line);
+                }
+
+                String finalJson = buffer.toString();
+                return finalJson;
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                if(connection!=null){
+                    connection.disconnect();
+                }
+
+                if(reader!=null){
+                    try {
+                        reader.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            // execution of result of Long time consuming operation
+            if(result!=null){
+                Log.w("the results are ", result);
+            }else{
+                Log.w("ERROR: ","Something Went Wrong at Server");
+            }
+
+
+        }
+    }
+
 }
+
